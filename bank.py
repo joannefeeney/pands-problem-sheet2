e6168c88724254d436af497a24c47b8c90582e6e
Joanne <G00283773@atu.ie>
2023-04-10 23:30:50 +0100

    week 02 task

--- bank.py
+++ bank.py
@@ -5,13 +5,21 @@
 # Prints out the answer in a human readable format 
 # With a euro sign and decimal point between the euro and cent of the amount
 
+
+# First integer which will be input by user
 cent1 = int(input("Enter the first amount in cent: "))
+
+# Second integer which will be input by user
 cent2 = int(input("Enter the second amount in cent: "))
+
 
 total_cent = cent1 + cent2
 total_euro = 0
+# Use of if loop here to ensure it counts in euros if higher than 100
 if total_cent >= 100:
     total_euro += 1
     total_cent -= 100
 
+# Output formatted with {:.2f} so the number will be formatted with two decimal places
+# https://www.freecodecamp.org/news/2f-in-python-what-does-it-mean/ 
 print("The total amount is €{:.2f}".format(total_euro + total_cent/100))
