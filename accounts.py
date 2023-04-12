ef9ae03bfedfe110f8776e781d406b9f87df802e
Joanne <G00283773@atu.ie>
2023-04-12 20:41:14 +0100

    week03 task

--- accounts.py
+++ accounts.py
@@ -3,6 +3,14 @@
 # This script reads in a 10 character account number and outputs the account number 
 # with only the last 4 digits showing (and the first 6 digits replaced with Xs)
 
+'''
+What I think should be done to modify the program to allow any amount of integers
+is done below, where the first 6 digits are always covered by an X thanks to slicing. 
+There is probably a better way to do this with a loop where only the last 
+4 digits are shown for any length of input whereas mine just blocks out the first
+6 digits regardless on length
+'''
+
 # User input
 # https://www.w3schools.com/python/python_user_input.asp
 account_number = input("Please enter your 10 digit account number: ")
