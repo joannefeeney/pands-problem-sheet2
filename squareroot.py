9b3586f03d64170458495a1791f755cc942b7e99
Joanne <G00283773@atu.ie>
2023-04-13 14:35:56 +0100

    week06 task

---
+++ squareroot.py
@@ -0,0 +1,15 @@
+# squareroot.py
+# Author: Joanne Feeney
+# This program takes a positive floating-point number 
+# as input and outputs an approximation of its square root
+
+number = int(input(f"Enter a positive number: "))
+
+# You should create a function called <tt>sqrt</tt> that takes a positive 
+# floating-point number as input and outputs an approximation of its square root
+
+
+
+
+
+print("The square root of {} is approx. {}")
