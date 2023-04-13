62c2d09880d341a06e95b500dffff00e1b2b47b2
Joanne <G00283773@atu.ie>
2023-04-13 18:07:55 +0100

    week 07 task

---
+++ es.py
@@ -0,0 +1,13 @@
+# squareroot.py
+# Author: Joanne Feeney
+# This program reads in a text file and outputs the number of e's it contains 
+
+# Think about what is being asked here, document any assumptions you are making
+# The program should take the filename from an argument on the command line
+
+# How to open/read etc. a txt file 
+# https://www.w3schools.com/python/python_file_handling.asp
+
+
+f = open("", "r")
+print(f.read())
