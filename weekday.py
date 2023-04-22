00ef84747094a3f64f71cbd78367cb5ab0c78b0e
Joanne <G00283773@atu.ie>
2023-04-22 17:31:02 +0100

    week 05 task

--- weekday.py
+++ weekday.py
@@ -10,7 +10,7 @@ import datetime
 x = datetime.datetime.now()
 
 # Used the below site to help me create an if loop that reads in the day of the week
-# as an int and then output whether today is a weekday or weekend day
+# as an int and then output whether today is a week day or weekend day
 # https://pynative.com/python-get-the-day-of-week/
 # Was thinking I would have to use '%w' however this site showed me I don't have to
 no = x.weekday()
