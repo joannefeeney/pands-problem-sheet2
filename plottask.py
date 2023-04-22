27b8267d1d8e242909d2003ed5a4cf810658ac0c
Joanne <G00283773@atu.ie>
2023-04-22 18:16:35 +0100

    Week 08 task

--- plottask.py
+++ plottask.py
@@ -1,27 +1,34 @@
-# squareroot.py
+# plottask.py
 # Author: Joanne Feeney
 # This program displays a histogram of a normal distribution of 
 # a 1000 values with a mean of 5 and standard deviation of 2, 
 # and a plot of the function  h(x)=x3  in the range [0, 10], on the one set of axes
 
-# Some marks will be given for making the plot look nice (legend etc).
 
-
-#Lecture 8.2
+# Lecture 8.2 used for a lot of hte below code
 # https://www.w3schools.com/python/numpy/numpy_intro.asp
 # https://www.w3schools.com/python/matplotlib_intro.asp
 import numpy as np
 import matplotlib.pyplot as plt
 
-xpoints = np.array(range(1,101))
+xpoints = np.array(range(1,10))
 ypoints = xpoints * xpoints
 
-plt.plot(xpoints, ypoints, label= "putwhatIwant")
+# To change the colour of the line to red via '-r' I used:
+# https://www.w3schools.com/python/matplotlib_markers.asp
+plt.plot(xpoints, ypoints, '-r', label= "h(x)=x3")
 plt.legend()
 
 np.random.seed(1)
-normData = np.random.normal(size=1000)
-
+# To get stndard deviation and mean
+# https://www.w3schools.com/python/matplotlib_histograms.asp
+normData = np.random.normal(5, 2, 1000)
 plt.hist(normData)
 
-plt.show()  6485
+# To add a title
+# https://www.w3schools.com/python/matplotlib_labels.asp
+plt.title("Week 08 Task")
+# To add a grid
+# https://www.w3schools.com/python/matplotlib_grid.asp
+plt.grid(axis = 'y')
+plt.show()
